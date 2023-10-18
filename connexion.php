5c782c6fc55c030d4b14f51e91511d20bbb14289
tiotsop <tiotsop@gmail.com>
2023-10-18 20:51:51 +0100

    Modification of connexion file

---
+++ connexion.php
@@ -0,0 +1,7 @@
+<?php
+
+$link = mysqli_connect("localhost", "admin", "tiotsoptsona", "ecole");
+                // Check connection
+                if ($link === false) {
+                    die("ERROR: Could not connect. " . mysqli_connect_error());
+                }
