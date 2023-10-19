40ef1ecd26bcaecb5d1e4c4cd17115051cf85f67
lian <lianfunue@gmail.com>
2023-10-19 14:36:03 +0100

    adding and modifying students

---
+++ confirmation.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <?php
+            $nom = "";
+            $datenaissance = " ";
+            $matricule = " ";
+            $email = " ";
+            $sexe = " ";
+            $sportif = " ";
+            $id = " ";
+            $idfiliere = " ";
+
+            if (isset($_POST['filiere'])) {
+                $idfiliere = $_POST['filiere'];
+            }
+            if (isset($_POST['id'])) {
+                $id = $_POST['id'];
+            }
+            if (isset($_POST['nom'])) {
+                $nom = $_POST['nom'];
+            }
+            if (isset($_POST['datenaissance'])) {
+                $datenaissance = $_POST['datenaissance'];
+            }
+            if (isset($_POST['matricule'])) {
+                $matricule = $_POST['matricule'];
+            }
+            if (isset($_POST['email'])) {
+                $email = $_POST['email'];
+            }
+            if (isset($_POST['genre'])) {
+                $sexe = $_POST['genre'];
+            }
+            if (isset($_POST['sportif'])) {
+                $sportif = 1;
+            }else{ 
+                $sportif = 0;
+            }
+
+            $link = mysqli_connect("localhost","root","","schools");
+            if ($link === false) {
+                die("ERROR : could not connect.". mysqli_connect_error());
+            }
+
+            if (strlen($id) == 0) {
+                $sql = "INSERT INTO etudiant(nom, matricule, date_naissance, sportif, email, sexe, idfiliere) 
+                VALUES('$nom', '$matricule', '$datenaissance', '$sportif', '$email', '$sexe', '$idfiliere') ";
+                echo $sql;
+                if (mysqli_query($link, $sql)) {
+                    $last_id = mysqli_insert_id($link);
+                    echo '<h2> Enregistrement de '.$nom.' effectué avec success </h2><br>';
+                    echo '<div class="form_group"> </div>';
+                    echo '<a class="btn btn-primary" href="etudiant.php">Nouveau </a>';
+                    echo '<a class="btn btn-success" href="etudiant.php?idetudiant='.$last_id.'">Modifier </a>';
+                    echo '<a class="btn btn-dark" href="listeEtudiant.php">Voir les etudiants</a>';
+                    echo '</div>';
+                }else{
+                    echo "ERROR could not be able to execute $sql.". mysqli_error($link);
+                }
+            }else {
+                $sql = "UPDATE etudiant SET nom = '$nom', matricule = '$matricule', date_naissance='$datenaissance', sportif='$sportif', 
+                        email='$email', sexe='$sexe', idfiliere='$idfiliere' WHERE id ='$id' ";
+                if (mysqli_query($link, $sql)) {
+                    $last_id = mysqli_insert_id($link);
+                    echo '<h2> Modification de '.$nom.' effectuée avec success </h2><br>';
+                    echo '<div class="form_group"> </div>';
+                    echo '<a class="btn btn-primary" href="etudiant.php">Nouveau </a>';
+                    echo '<a class="btn btn-success" href="etudiant.php?idetudiant='.$id.'">Modifier </a>';
+                    echo '<a class="btn btn-dark" href="listeEtudiant.php">Voir les etudiants</a>';
+                    echo '</div>';
+                }else{
+                    echo "ERROR could not be able to execute $sql.". mysqli_error($link);
+                }
+            }
+
+            mysqli_close($link);
+        ?>
+        
+    </div>
+</body>
+</html>
--- etudiant.php
+++ etudiant.php
@@ -1,57 +1,112 @@
+<?php 
+    error_reporting(E_ERROR);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bootstrap.css">
     <title>Document</title>
 </head>
 <body>
-<?php
-include 'connexion.php';
-//session_start();
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 offset-md-1">
+                <h1>Enregistrement d'un étudiant</h1>
 
-if(isset($_POST['submit'])){
+                <?php
+                    if (isset($_GET['idetudiant'])) {
+                        $nom = "";
+                        $id = $_GET['idetudiant'];
+                        $link = mysqli_connect("localhost","root","","schools");
+                        if ($link === false) {
+                            die("ERROR : could not connect.". mysqli_connect_error());
+                        }
+                        $requete = "SELECT * FROM etudiant WHERE id=$id";
+                        $resultat = mysqli_query($link, $requete);
+                        $row = mysqli_fetch_assoc($resultat);
+                    }
+                ?>
 
-   $name = $_POST['name'];
-   $name = filter_var($name, FILTER_SANITIZE_STRING);
-   $mat = $_POST['mat'];
-   $mat = filter_var($mat, FILTER_SANITIZE_STRING);
-   $filiere = $_POST['filiere'];
-   $filiere = filter_var($filiere, FILTER_SANITIZE_STRING);
-   $date = $_POST['date'];
-   $date = filter_var($date, FILTER_SANITIZE_STRING);
-   $sport = $_POST['sport'];
-   $sport = filter_var($sport, FILTER_SANITIZE_STRING);
-   $email = $_POST['email'];
-   $email = filter_var($email, FILTER_SANITIZE_STRING);
-   $sexe = $_POST['sex'];
-   $sexe = filter_var($sexe, FILTER_SANITIZE_STRING);
+                <form method="post" action="confirmation.php">
+                    <input hidden value="<?php echo $row['id'] ?>" type="text" name="id">
+                    <fieldset>
+                        <div class="form-group">
+                            <label class="col-form-label" for="nom">Nom</label>
+                            <input value="<?php echo $row['nom'] ?>" type="text" class="form-control" id="nom" name="nom" placeholder="Votre nom ....">
+                        </div>
+                        <div class="form-group">
+                            <label class="col-form-label" for="date naissance">Date naissance</label>
+                            <input value="<?php echo $row['date_naissance'] ?>"  type="date" class="form-control" id="date" name="datenaissance">
+                        </div>
+                        <div class="form-group">
+                            <label class="col-form-label" for="matricule">Matricule</label>
+                            <input value="<?php echo $row['matricule'] ?>" type="text" class="form-control" id="matricule" name="matricule" placeholder="Votre matricule ....">
+                        </div>
+                        <div class="form-group">
+                            <label class="col-form-label" for="Filière">Filière</label>
+                            <select name="filiere" id="filiere" class="form-select">
+                                <option value="">Selectionner votre filère.... </option>
+                                <?php
+                                    $link = mysqli_connect("localhost","root","","schools");
+                                    if ($link === false) {
+                                        die("ERROR : could not connect.". mysqli_connect_error());
+                                    }
+                                    $requete = "SELECT * FROM filiere ORDER BY libelle";
+                                    $resultat = mysqli_query($link, $requete);
+                                    while ($row1 = mysqli_fetch_assoc($resultat)) {
+                                        ?>
+                                        <option <?php if( $row['idfiliere'] == $row1['idfiliere']){ 
+                                            echo 'selected';
+                                        }  ?> value="<?php echo $row1['idfiliere'] ?>">
+                                        <?php
+                                        echo $row1['libelle']
+                                        ?>
+                                        </option>
+                                        <?php
+                                    }
 
-   $sql = "INSERT into etudiant values('$name','$mat','$filiere','$date','$sport','$email','$sexe')";
+                                ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label class="col-form-label" for="Email">Email</label>
+                            <input value="<?php echo $row['email'] ?>" type="email" class="form-control" id="email" name="email" placeholder="Adresse email ....">
+                        </div>
+                        <div class="form-group">
+                            <label class="col-form-label" for="genre">Sexe</label>
+                            <select name="genre" id="genre" class="form-select">
+                                <option value="">Selectionner votre Genre.... </option>
+                                <option value="M" <?php if ($row['sexe']=='M') {
+                                    echo 'selected';
+                                } ?> > Masculin </option>
+                                <option value="F" <?php if ($row['sexe']=='F') {
+                                    echo 'selected';
+                                } ?>  > Feminin </option>
+                            </select>
+                        </div>
+                        <div class="form-check">
+                            <label class="col-form-label">
+                                <input <?php if ($row['sportif'] == 1) {
+                                    echo 'checked';
+                                } ?> type="checkbox" name="sportif" class="form-check-input">
+                                Sportif
+                            </label>                            
+                        </div>
 
-    header("location:listeEtudiant.php");
-   
-}
+                        <br>
 
-?>
-<section class="form-container">
+                        <div class="form-group">
+                            <input class="btn btn-danger" type="reset" value="annuler">
+                            <input class="btn btn-success" type="submit"  value="enregistrer">
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
 
-<form action="listeEtudiant.php" method="post">
-  <h3>Register student</h3> 
- <p>Nom <input type="text" name="name" class="box"></p><br>
-  <p>Matricule <input type="text" name="mat" class="box"></p><br>
-  <p>Filiere <input type="text" name="filiere" class="box"></p><br>
-  <p>Date de naissance <input type="date" name="date" class="box"></p><br>
-  <p>Sportif  <input type="text" name="sport"></p><br>
-           <p>Email <input type="email" name="email" class="box"></p><br>
-           <p>Sexe <input type="text" name="sexe" class="box"></p><br>
-           <p><input type="submit" value="Submit"><br>
-
-</form>
-
-    </section>
-
-    
+        </div>
+    </div>
 </body>
 </html>
